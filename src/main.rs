8765e671ebe0c002f6c1d59cea6e2e73a0118e29
Chris Woods <chris.woods@gmail.com>
2021-12-17 21:34:09 -0500

    intro to methods

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,21 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
-fn main() {
-    let scale = 2;
-    let rect1 = Rectangle {
-        width: dbg!(30 * scale),
-        height: 50,
-    };
-    dbg!(&rect1);
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    )
 }
